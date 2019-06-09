f06c806726ae3091aec1190d3a44355db2494f34
BelleChoucroute <chloemancon@hotmail.fr>
2019-06-09 23:04:22 +0200

    🌐🇫🇷 Some French messages added
    Some French messages added  :
    - 8 "Message of the Week",
    - 4 "Talking to myself".

---
+++ data/fr/messageOfTheWeekData.js
@@ -0,0 +1,34 @@
+module.exports = [
+//6
+	`Le monde appartient à
+ceux qui se lèvent tôt.
+Mais ceux qui se lèvent
+tard sont moins fatigués!`,
+//76
+	`On n'a généralement
+qu'une chance.
+\nSauf en cas d'amnésie!`,
+//81
+	`Ça fait mal de réfléchir.
+\nJ'essaie d'arrêter.`,
+//87
+	`Si tu perds quelque chose
+de coûteux,
+dis que c'est la
+faute des pirates.`,
+//102
+	`Un ami dans le besoin,
+il faut l'aider.
+Un ennemi dans le besoin,
+il faut l'enfoncer.`,
+//114
+	`Lorsqu'il enfile son
+masque, le catcheur
+pleure son amour perdu.`,
+//120
+	`Quand il pleut, ça sauce.
+\nApporte ton parapluie.`,
+//124
+	`Apprendre ne peut
+pas faire de mal.`
+];
