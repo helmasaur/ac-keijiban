502a5df6dde6e95fd460c0ed7af083916f526786
BelleChoucroute <48658922+BelleChoucroute@users.noreply.github.com>
2019-06-09 23:10:14 +0200

    🌐🇫🇷 Some French messages added (#19)
    Some French messages added  :
    - 8 "Message of the Week",
    - 4 "Talking to myself".

---
+++ data/fr/talkingToMyselfData.js
@@ -0,0 +1,26 @@
+module.exports = [
+//50
+	`J'adorais les pigeons
+voyageurs quand j'étais
+petite. Ça doit être
+sympa d'être payé pour
+remettre des messages.`,
+//106
+	`Qu'est-ce qui est le pire?
+Fuir un problème avant
+qu'il ne survienne ou tout
+laisser tomber en plein
+milieu? Pour savoir.`,
+//126
+	`Thibou dit qu'avec moi, le
+problème, c'est que
+j'utilise trop mon
+"hémisphère droit."
+Il est gaucher?!`,
+//154
+	`En quelle couleur je
+devrais décolorer mes
+plumes? Le rouge, c'est
+trop voyant. Le bleu, ça
+mincit. Je vais voir`
+];
