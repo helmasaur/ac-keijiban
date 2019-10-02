59f6b469263037c3448369bd68e0b75b00ab7a06
BelleChoucroute <chloemancon@hotmail.fr>
2019-10-02 19:23:00 +0200

    🌐🇫🇷 Some French messages added
    Some French messages added  in "Message of the Week" and "Talking to myself".

--- data/fr/talkingToMyselfData.js
+++ data/fr/talkingToMyselfData.js
@@ -1,10 +1,27 @@
 module.exports = [
+//26
+	`Quand je compte mon
+argent au magasin, les
+gens me demandent
+si j'aime mon travail.
+Ça me rend triste.`,
 //50
 	`J'adorais les pigeons
 voyageurs quand j'étais
 petite. Ça doit être
 sympa d'être payé pour
 remettre des messages.`,
+//54
+	`Quand je vois ma sœur
+parler à Antoine, je me
+sens toute bizarre…
+Peut-être un truc qui
+me reste sur l'estomac.`,
+//98
+	`On dit que les vers
+ rampent, mais je ne
+comprends pas. Ils
+n'ont pas de pattes!`,
 //106
 	`Qu'est-ce qui est le pire?
 Fuir un problème avant
@@ -17,10 +34,42 @@ problème, c'est que
 j'utilise trop mon
 "hémisphère droit."
 Il est gaucher?!`,
+//134
+	`Je suis très connue à
+la section Enfants de
+la bibliothèque… Pff…
+Oh, Antoine…`,
+//142
+	`Ce serait bien que les
+gens soient fournis avec
+un mode d'emploi. J'en
+prendrais un pour ma
+soeur et 5 pour Antoine!`,
 //154
 	`En quelle couleur je
 devrais décolorer mes
 plumes? Le rouge, c'est
 trop voyant. Le bleu, ça
-mincit. Je vais voir`
+mincit. Je vais voir`,
+//158
+	`Combien de tomates
+cerises au dîner? Trois?
+Douze? Non, c'est trop.
+Je vais en prendre quatre.`,
+//174
+	`Hip hop bididi hop…
+Hip hop bididi hop…
+Cette chanson est connue
+partout sauf ici! Pff !`,
+//178
+	`Astrid m'a dit que le
+pélican était le symbole
+de l'économie.
+Je ne suis pas sûre
+d'être d'accord.`,
+//221
+	`J'envie vraiment les
+comiques. Même quand ils
+n'ont pas envie de rire,
+ils font rire les autres.`
 ];
