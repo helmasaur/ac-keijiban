46b3b8959b595368ff5342fc2ee8a2b6182e4c61
BelleChoucroute <48658922+BelleChoucroute@users.noreply.github.com>
2019-10-27 20:16:40 +0100

    🌐🇫🇷 Some French messages added (#27)
    * 🌐🇫🇷 Some French messages added
    
    Some French messages added  :
    - 8 "Message of the Week",
    - 4 "Talking to myself".
    
    * 🌐🇫🇷 Some French messages added
    
    Some French messages added  in "Message of the Week" and "Talking to myself".

--- data/fr/messageOfTheWeekData.js
+++ data/fr/messageOfTheWeekData.js
@@ -4,6 +4,21 @@ module.exports = [
 ceux qui se lèvent tôt.
 Mais ceux qui se lèvent
 tard sont moins fatigués!`,
+//52
+	`Verser des larmes,
+de la sueur
+et du sang
+est moins facile
+qu'il n'y paraît!!`,
+//56
+	`Appelle les pompiers.
+\nAvant que la maison 
+ne brûle.`,
+//68
+	`Les nuages volent
+haut dans le ciel.
+Mais qui est
+aux commandes?`,
 //76
 	`On n'a généralement
 qu'une chance.
@@ -16,19 +31,50 @@ qu'une chance.
 de coûteux,
 dis que c'est la
 faute des pirates.`,
+//91
+	`Tu ne sais pas dire
+des choses
+gentilles?
+Ecris-les!`,
+//98
+	`Emporte toujours un
+short de rechange.
+\nFais-moi confiance!`,
 //102
 	`Un ami dans le besoin,
 il faut l'aider.
 Un ennemi dans le besoin,
 il faut l'enfoncer.`,
+//111
+	`Une pomme le matin
+éloigne le médecin.
+Un oignon le matin
+éloigne tout le monde.`,
 //114
 	`Lorsqu'il enfile son
 masque, le catcheur
 pleure son amour perdu.`,
+//119
+	`Trouver un centime
+et le ramasser.
+\nQuelle perte de temps.`,
 //120
 	`Quand il pleut, ça sauce.
 \nApporte ton parapluie.`,
 //124
 	`Apprendre ne peut
-pas faire de mal.`
+pas faire de mal.`,
+//134
+	`Ha, ha, ha.
+Hé, hé, hé.
+Les pièges,
+j'adore.`,
+//141
+	`Le génie, c'est 1%
+d'inspiration et 99%
+de sueur.
+Imagine l'odeur.`,
+//150
+	`Un poil dans la main…
+\nça doit faire mal.`
 ];
